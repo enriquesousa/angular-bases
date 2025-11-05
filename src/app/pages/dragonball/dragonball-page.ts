c1222e28491cc854cda0f8b8ab361948d606c65e
Enrique <enrique.sousa@gmail.com>
2025-11-04 19:21:45 -0800

    52. ngClass - Clases de CSS de forma condicional

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character {
     id: number;
@@ -7,7 +8,7 @@ interface Character {
 }
 
 @Component({
-  imports: [],
+  imports: [ NgClass ],
   templateUrl: './dragonball-page.html',
   styleUrl: './dragonball-page.css',
 })
@@ -19,5 +20,11 @@ export class DragonballPageComponent {
         { id: 1, name: 'Piccolo', power: 3000 },
         { id: 1, name: 'Trunks', power: 9000 },
     ]);
+
+    powerClasess = computed( () => {
+        return {
+            'text-danger': true,
+        }
+    });
 
 }
