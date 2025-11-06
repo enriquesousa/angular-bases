f8bcc32b02f37c18ba2e9182f4316293ba0181b2
Enrique <enrique.sousa@gmail.com>
2025-11-06 11:58:23 -0800

    54. inputs - Agregar Personaje

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -14,6 +14,9 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+    name = signal('Gohan');
+    power = signal(1000);
+
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 9001 },
         { id: 2, name: 'Vegueta', power: 8000 },
@@ -27,5 +30,13 @@ export class DragonballPageComponent {
             'text-danger': true,
         }
     });
+
+    addCharacter = () => {
+        console.log(this.name(), this.power());
+        // this.characters.set([
+        //     ...this.characters(),
+        //     { id: this.characters().length + 1, name: this.name(), power: this.power() }
+        // ]);
+    }
 
 }
